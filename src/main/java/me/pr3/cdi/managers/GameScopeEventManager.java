62f158b7108ef6e8c44a2790b4d6a387acc69631
tim <pr3roxdlc.yt@web.de>
2023-01-17 22:15:03 +0100

    More Base Scopes

--- src/main/java/me/pr3/cdi/managers/GameScopeEventManager.java
+++ src/main/java/me/pr3/cdi/managers/GameScopeEventManager.java
@@ -3,9 +3,14 @@ package me.pr3.cdi.managers;
 import me.pr3.cdi.annotations.scopes.LifeScoped;
 import me.pr3.cdi.annotations.scopes.SessionScoped;
 import me.pr3.cdi.annotations.scopes.WorldScoped;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+
+import static me.pr3.cdi.managers.ScopeManager.INSTANCE;
 
 public class GameScopeEventManager {
     public GameScopeEventManager(){
@@ -15,9 +20,20 @@ public class GameScopeEventManager {
     @SubscribeEvent
     public void onServerConnectedEvent(FMLNetworkEvent.ClientConnectedToServerEvent e) {
         System.out.println("Connected to Server: " + e.getConnectionType());
-        ScopeManager.INSTANCE.initScope(SessionScoped.class);
-        ScopeManager.INSTANCE.initScope(WorldScoped.class);
-        ScopeManager.INSTANCE.initScope(LifeScoped.class);
+        INSTANCE.initScope(SessionScoped.class);
+        INSTANCE.initScope(WorldScoped.class);
+        INSTANCE.initScope(LifeScoped.class);
+    }
+
+    @SubscribeEvent
+    public void onPlayerDeath(LivingDeathEvent e) {
+        if (e.getEntity().equals(Minecraft.getMinecraft().player)) {
+            INSTANCE.initScope(LifeScoped.class);
+        }
+    }
+
+    public void onWorldChange(PlayerEvent.PlayerChangedDimensionEvent event){
+        INSTANCE.initScope(WorldScoped.class);
     }
 
 }
